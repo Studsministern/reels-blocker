afa92182f90f9c7bc6ef0159d5ca56549841dc08
Eric Weidow <eric@weidow.se>
2022-12-23 15:35:07 +0100

    Speed up hiding menus, hides more YouTube Shorts
    Speeds up the hiding of the YouTube Shorts menus. Also hides the
    sections of Shorts in the home tab and the Shorts in subscriptions.
    
    However, instead of just hiding Shorts in subscriptions, they should be
    removed entirely. This is because they still affect the structure of
    videos around them.

--- Chrome Extension/scripts/youtube.js
+++ Chrome Extension/scripts/youtube.js
@@ -1,26 +1,37 @@
 (() => {
-    function hide(selector) {
-        const html = document.querySelector(selector);
-        if(html && !html.classList.contains('hide')) html.classList.add('hide');
-    }
+    /** CSS selectors to get rid of YouTube Shorts, in order: 
+     * YouTube Shorts mini side menu
+     * YouTube Shorts large side menu
+     * YouTube Shorts section on home screen
+     * YouTube Shorts in subscriptions
+     * 
+     * Important to use \" instead of just "
+     */ 
 
-    function hideHTML() {
-        // YouTube shorts menus:
-        hide('ytd-mini-guide-entry-renderer[aria-label=\"Shorts\"]');
-        hide('ytd-guide-entry-renderer:has([title=\"Shorts\"])');
-    };
-    
-    $('html').append(`
-        <style>
-        .hide {
+    const selectorArray = [
+        'ytd-mini-guide-entry-renderer[aria-label=\"Shorts\"]',
+        'ytd-guide-entry-renderer:has([title=\"Shorts\"])',
+        'ytd-rich-section-renderer:has(ytd-rich-shelf-renderer[is-shorts])',
+        'ytd-grid-video-renderer:has(span.ytd-thumbnail-overlay-time-status-renderer[aria-label=\"Shorts\"])'
+    ];
+
+    const stringifiedArray = (() => {
+        let string = selectorArray[0];
+
+        for(let i = 1; i < selectorArray.length; i++) {
+            string += `,\n${selectorArray[i]}`;
+        }
+
+        return string;
+    })();
+
+    $('html').append(
+        `<style>
+        ${stringifiedArray} {
             visibility: collapse!important;
             height: 0!important;
             width: 0!important;
         }
         </style>`
     );
-    
-    window.addEventListener("load", hideHTML);
-    //window.addEventListener("yt-page-data-updated", hideHTML);
-    //window.addEventListener("state-navigateend", hideHTML);
 })();
