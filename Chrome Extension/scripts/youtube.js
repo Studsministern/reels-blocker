ffd1272fad0c3d85177ddbc45bdea132900c3017
Eric Weidow <eric@weidow.se>
2022-12-23 16:13:41 +0100

    Hide Shorts in search results

--- Chrome Extension/scripts/youtube.js
+++ Chrome Extension/scripts/youtube.js
@@ -1,9 +1,12 @@
 (() => {
-    /** CSS selectors to get rid of YouTube Shorts, in order: 
-     * YouTube Shorts mini side menu
-     * YouTube Shorts large side menu
-     * YouTube Shorts section on home screen
-     * YouTube Shorts in subscriptions
+    /** CSS selectors to get rid of YouTube Shorts: 
+     * 0: YouTube Shorts mini side menu
+     * 1: YouTube Shorts large side menu
+     * 2: YouTube Shorts section on home screen
+     * 3: YouTube Shorts in subscriptions
+     * 4: YouTube Shorts section in search results
+     * 5: YouTube Shorts in search results (based on aria label for video title, case insensitive)
+     * 6: YouTube Shorts in search results (based on overlay-style for "shorts" logo over video thumbnail)
      * 
      * Important to use \" instead of just "
      */ 
@@ -12,7 +15,10 @@
         'ytd-mini-guide-entry-renderer[aria-label=\"Shorts\"]',
         'ytd-guide-entry-renderer:has([title=\"Shorts\"])',
         'ytd-rich-section-renderer:has(ytd-rich-shelf-renderer[is-shorts])',
-        'ytd-grid-video-renderer:has([overlay-style=\"SHORTS\"])'
+        'ytd-grid-video-renderer:has([overlay-style=\"SHORTS\"])',
+        'ytd-reel-shelf-renderer',
+        'ytd-video-renderer:has([aria-label*="#shorts"i])',
+        'ytd-video-renderer:has([overlay-style="SHORTS"])'
     ];
 
     const stringifiedArray = (() => {
