d12262fc00930b7565d56e7edb6abee3727e5e69
Eric Weidow <eric@weidow.se>
2022-12-25 14:53:13 +0100

    Hide explore page instead of limiting number of reels

--- Chrome Extension/scripts/instagram.js
+++ Chrome Extension/scripts/instagram.js
@@ -1,10 +1,12 @@
 /** CSS selectors in selectorArray to get rid of infinite scrolling and reels: 
- * 0: Hiding "Suggested posts" text on home screen
- * 1: Hiding the actual suggested posts on home screen
- * 2: Hiding loading logo for more posts (not removing it results in infinite loading under suggested posts!)
- * 3: Hiding "More posts from ____" under posts (instagram.com/p/*) and reels (instagram.com/reel/*)
- * 4: Hiding reels menu tab on a page
- * 5: Hiding reels on a page (instagram.com/pagename/reels/*)
+ * 0: Hiding home screen "Suggested posts" text
+ * 1: Hiding home screen suggested posts
+ * 2: Hiding home screen loading logo (not removing it results in infinite loading under suggested posts!)
+ * 3: Hiding posts/reels "More posts from ____" (on instagram.com/p/* and instagram.com/reel/*)
+ * 4: Hiding page reels menu tab
+ * 5: Hiding page reels (instagram.com/pagename/reels/*)
+ * 6: Hiding explore page menu tab
+ * 7: Hiding explore page posts/reels
  * 
  * Seems important to use \" instead of just " in the selectorArray below!
  */ 
@@ -15,17 +17,10 @@ const selectorArray = [
     'main div._aalg',
     'main>div>div~*[class*=\"_aa6g\"]',
     'main a[role=\"tab\"][href$=\"reels/\"]',
-    'main div[style*="position: relative; display: flex; flex-direction: column;"]:has(div[class=\"_ac7v _abq4\"])'
+    'main div[style*="position: relative; display: flex; flex-direction: column;"]:has(div[class=\"_ac7v _abq4\"])',
+    'div:not([class]):has(>div>a[href=\"/explore/\"])',
+    'main div[style*="position: relative; display: flex; flex-direction: column;"]:has(div[class=\" _ab8x  _ab94 _ab99 _ab9f _ab9m _ab9p _abcm\"])'
 ];
-
-// Number of sections in the explore page that shows up
-const explorePageSections = 3;
-
-if(explorePageSections > 0) {
-    selectorArray.push(
-        `main div[style*="position: relative; display: flex; flex-direction: column;"]:has(div[class=" _ab8x  _ab94 _ab99 _ab9f _ab9m _ab9p _abcm"])>:nth-child(n + ${explorePageSections + 1})`,
-    );
-}
 
 // Convert the selector array to a string, for use as a CSS selector
 const stringifiedArray = (() => {
