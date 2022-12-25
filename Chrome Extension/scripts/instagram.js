c321bf57f919fe188674d20489940b0fe1d6853f
Eric Weidow <eric@weidow.se>
2022-12-25 13:38:35 +0100

    Hide reels on instagram pages

--- Chrome Extension/scripts/instagram.js
+++ Chrome Extension/scripts/instagram.js
@@ -3,6 +3,8 @@
  * 1: Hiding the actual suggested posts on home screen
  * 2: Hiding loading logo for more posts (not removing it results in infinite loading under suggested posts!)
  * 3: Hiding "More posts from ____" under posts (instagram.com/p/*) and reels (instagram.com/reel/*)
+ * 4: Hiding reels menu tab on a page
+ * 5: Hiding reels on a page (instagram.com/pagename/reels/*)
  * 
  * Seems important to use \" instead of just " in the selectorArray below!
  */ 
@@ -11,7 +13,9 @@ const selectorArray = [
     'main div[style*=\"position: relative; display: flex; flex-direction: column;\"]>div:not([class])>:last-child',
     'main div[style*=\"position: relative; display: flex; flex-direction: column;\"]>div:not([class])~article',
     'main div._aalg',
-    'main>div>div~*[class*=\"_aa6g\"]'
+    'main>div>div~*[class*=\"_aa6g\"]',
+    'main a[role=\"tab\"][href$=\"reels/\"]',
+    'main div[style*="position: relative; display: flex; flex-direction: column;"]:has(div[class=\"_ac7v _abq4\"])'
 ];
 
 const stringifiedArray = (() => {
@@ -24,6 +28,7 @@ const stringifiedArray = (() => {
     return string;
 })();
 
+// Add the CSS styling
 $('html').prepend(
     `<style>
     ${stringifiedArray} {
