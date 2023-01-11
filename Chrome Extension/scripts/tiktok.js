c6be783ad80f627d19cf9954e2100e7db7c1d5ee
Eric Weidow <eric@weidow.se>
2023-01-11 22:21:45 +0100

    Hide TikTok infinite loading symbol on recommended

--- Chrome Extension/scripts/tiktok.js
+++ Chrome Extension/scripts/tiktok.js
@@ -1,12 +1,14 @@
 (() => {
     /** CSS selectors in selectorArray to get rid of Tiktoks: 
      * 0: Recommended page only showing one post
+     * 1: Removing loading symbol on recommended page 
      * 
      * Seems important to use \" instead of just " in the selectorArray below!
      */ 
 
     const selectorArray = [
-        '[data-e2e=\"recommend-list-item-container\"]~[data-e2e=\"recommend-list-item-container\"]'
+        '[data-e2e=\"recommend-list-item-container\"]~[data-e2e=\"recommend-list-item-container\"]',
+        'svg[class*=\"tiktok-qmnyxf-SvgContainer\"]'
     ];
 
     const stringifiedArray = (() => {
